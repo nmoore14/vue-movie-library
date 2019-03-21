51a972543b2d7972d383cb18ad391b74ed77e2a2
Nick Moore <nickmoore_1@live.com>
2019-03-21 13:31:52 -0400

    working on styling and adding items

--- src/components/MyNavDrawer.vue
+++ src/components/MyNavDrawer.vue
@@ -1,21 +1,58 @@
 <template>
-  <v-navigation-drawer 
-    v-model='drawer' 
-    absolute 
+  <v-navigation-drawer
+    v-model='drawer'
+    absolute
     temporary
     class="grey"
     >
+    <v-list class='grey lighten-3 pl-2'>
+      <v-list-tile-title class="title grey--text text--darken-4 font-weight-light">{{ username }}'s Library</v-list-tile-title>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title class="title grey--text text--darken-4 font-weight-light">Home</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <v-expansion-panel
+      expand
+      focusable
+    >
+      <v-expansion-panel-content
+        v-for="(top, index) in userTop"
+        :key="index"
+      >
+        <div slot="header" class="title grey--text text--darken-4 font-weight-light">{{ top.title }}</div>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+
   </v-navigation-drawer>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   name: 'MyNavDrawer',
   data () {
     return {
-      drawer: null
+      drawer: null,
+      username: 'Nick',
+      userTop: [
+        {
+          title: 'Recent',
+          icon: 'event'
+        },
+        {
+          title: 'Favorites',
+          icon: 'stars'
+        }
+      ]
     }
   },
   computed: {
